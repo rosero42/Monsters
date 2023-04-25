2acdeb83b02f71170855cd472d33532f42ae344a
rosero42 <rosvanis.r@gmail.com>
2023-04-24 21:55:05 -0700

    blank slate

--- src/App.js
+++ src/App.js
@@ -5,32 +5,12 @@ import './App.css';
 class App extends Component {
   constructor(){
     super();
-
-    this.state = {
-      name: {firstName: 'Yihua', lastName: 'Zhang'},
-      company: 'ztm'
-    }
   }
 
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
-          </p>
-          <button onClick={()=>{
-            //Always use the same type of value for set set
-            this.setState(()=>{
-              return{
-                name: {firstName: 'Andrea', lastName: 'Zimmerman'}
-              }
-            }, ()=>{
-              console.log(this.state)
-            })
-          }}>Change name</button>
-        </header>
+       
       </div>
     );
   }
