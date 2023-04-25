6c846081d0373e3bcae359564c23de82f731292b
rosero42 <rosvanis.r@gmail.com>
2023-04-24 22:44:15 -0700

    Add logs

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ class App extends Component {
     this.state={
       monsters:[],
     }
+    console.log('constructor')
   }
 
   componentDidMount(){
+    console.log('componentDidMount')
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then((users)=> this.setState(() =>{
@@ -23,6 +25,7 @@ class App extends Component {
   }
 
   render(){
+    console.log('render')
     return (
       <div className="App">
        {
