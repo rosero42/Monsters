999fa7b8277520d552f45b023387f73c06760a41
rosero42 <rosvanis.r@gmail.com>
2023-05-08 18:59:46 -0700

    Add card list to styles

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,9 +1,10 @@
 import { Component } from "react";
+import "./card-list.styles.css"
 class CardList extends Component{
     render(){
         const {monsters} = this.props;
         return(
-            <div>
+            <div className='card-list'>
                 {monsters.map(monster =>{
                     const {name, email, id} = monster;
                     return(  					
