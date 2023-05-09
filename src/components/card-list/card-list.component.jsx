d9ce4feb37d43aeb65ca92ae58ea8d100ecff207
rosero42 <rosvanis.r@gmail.com>
2023-05-08 18:55:37 -0700

    Add variables where necessary

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -4,18 +4,17 @@ class CardList extends Component{
         const {monsters} = this.props;
         return(
             <div>
-                {monsters.map(monster =>(
-                    <div className="card-container">
-                        <img alt={`monster ${monster.name}`}
-                            src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
-                        ></img>
-                        <h2>{monster.name}</h2>
-                        <p>{monster.email}</p>
-
-
-
-                    </div>
-                ))}
+                {monsters.map(monster =>{
+                    const {name, email, id} = monster;
+                    return(  					
+                        <div className="card-container" key={id}>
+                            <img alt={`monster ${name}`}
+                                 src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
+                            <h2>{name}</h2>
+                            <p>{email}</p>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
