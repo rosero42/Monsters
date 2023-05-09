20bbda42d28433566dee8e9284622e4b67972b5d
rosero42 <rosvanis.r@gmail.com>
2023-05-08 19:11:56 -0700

    Move card to its own component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,21 +1,14 @@
 import { Component } from "react";
+import Card from "../card/card.component";
 import "./card-list.styles.css"
 class CardList extends Component{
     render(){
         const {monsters} = this.props;
         return(
             <div className='card-list'>
-                {monsters.map(monster =>{
-                    const {name, email, id} = monster;
-                    return(  					
-                        <div className="card-container" key={id}>
-                            <img alt={`monster ${name}`}
-                                 src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
-                            <h2>{name}</h2>
-                            <p>{email}</p>
-                        </div>
-                    )
-                })}
+                {monsters.map(monster =>(
+                       <Card monster={monster}/>
+                ))}
             </div>
         )
     }
